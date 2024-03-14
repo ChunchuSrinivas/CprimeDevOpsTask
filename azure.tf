e542cf6ba7d0d6033146dd3f7948755675941ffa
ChunchuSrinivas <srinivas.chunchu02@gmail.com>
2024-03-14 18:36:59 +0000

    modified azure tf file

--- azure.tf
+++ azure.tf
@@ -1,8 +1,8 @@
 provider "azurerm" {
 	features {}
  	subscription_id = "895f0410-bb29-4612-b064-a8705da764df"
-	client_id = "8e5c6789-d85e-423f-8eca-da97b61d63d0"
-	client_secret = "q888Q~Ef61NPbOB1S6ahecWSBKYayWzTySZyscpC"
-	tenant_id = "a866d2ce-a71b-47d8-8134-7e8aab72fa46"
+	client_id       = "488b1f5c-f3b5-47e3-b9ee-9ef687d706d1"
+	client_secret   = "pyX8Q~4QFqityw2pzWDNDHtHZdnLDBt0jfga5aWd"
+	tenant_id       = "a866d2ce-a71b-47d8-8134-7e8aab72fa46"
 
 }
