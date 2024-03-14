08591928d99f32db319f47f292f81999141914df
ChunchuSrinivas <87190755+ChunchuSrinivas@users.noreply.github.com>
2024-03-14 23:11:10 +0530

    Created variable tf

---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable location {
+  type = string
+  default = "centralindia"
+  description = "location is central india"
+}
+
+variable resourcegroup {
+  type = string
+  default = "RgCprimeDevOpstask"
+  description = "rg name is RgCprimeDevOpstask"
+}
