bd7f02fd2a18a06fd754bb13b4130762b7ad5a79
ChunchuSrinivas <87190755+ChunchuSrinivas@users.noreply.github.com>
2024-03-14 23:12:26 +0530

    Updated main tf file

--- main.tf
+++ main.tf
@@ -1,4 +1,20 @@
-resource "azurerm_resource_group" "rgfrcprime" {
-    name     = "RgCprimeDevOpstask"
-    location = "centralindia"
+#Creating resource group
+resource "azurerm_resource_group" "myrgfrcprime" {
+    name     = var.resourcegroup
+    location = var.location
 }
+
+/*
+#creating storage account to store terraform statefile as backend
+resource "azurerm_storage_account" "example" {
+  name                     = "var.storageaccountnamecprimestorageaccount"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
+*/
