5d814116cfaa9386f68a7818f354ec4ef948aee8
ChunchuSrinivas <87190755+ChunchuSrinivas@users.noreply.github.com>
2024-03-14 21:23:53 +0530

    Create azure.tf

---
+++ azure.tf
@@ -0,0 +1,8 @@
+provider "azurerm" {
+	features {}
+ 	subscription_id = "895f0410-bb29-4612-b064-a8705da764df"
+	client_id = "8e5c6789-d85e-423f-8eca-da97b61d63d0"
+	client_secret = "q888Q~Ef61NPbOB1S6ahecWSBKYayWzTySZyscpC"
+	tenant_id = "a866d2ce-a71b-47d8-8134-7e8aab72fa46"
+
+}
