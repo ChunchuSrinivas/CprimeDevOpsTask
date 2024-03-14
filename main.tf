7852deef6c523a56a94d10977734e6e5241d161f
ChunchuSrinivas <srinivas.chunchu02@gmail.com>
2024-03-14 18:58:33 +0000

    added storage acnt resource details

--- main.tf
+++ main.tf
@@ -4,18 +4,16 @@ resource "azurerm_resource_group" "myrgfrcprime" {
     location = var.location
 }
 
-
-/*
 #creating storage account to store terraform statefile as backend
-resource "azurerm_storage_account" "example" {
-  name                     = "var.storageaccountnamecprimestorageaccount"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+resource "azurerm_storage_account" "cprimestacnt" {
+  name                     = var.storageaccountname
+  resource_group_name      = var.resourcegroup
+  location                 = var.location
   account_tier             = "Standard"
-  account_replication_type = "GRS"
+  account_replication_type = "LRS"
 
   tags = {
-    environment = "staging"
+    environment = "dev"
   }
 }
-*/
+
--- variable.tf
+++ variable.tf
@@ -1,11 +1,17 @@
 variable location {
-  type = string
-  default = "centralindia"
+  type        = string
+  default     = "centralindia"
   description = "location is central india"
 }
 
 variable resourcegroup {
-  type = string
-  default = "RgCprimeDevOpstask"
+  type        = string
+  default     = "RgCprimeDevOpstask"
   description = "rg name is RgCprimeDevOpstask"
+}
+
+variable storageaccountname {
+  type        = string
+  default     = "CprimeStorageAcnt"
+  description = "Cprime DevOps Task Storage Account"
 }
